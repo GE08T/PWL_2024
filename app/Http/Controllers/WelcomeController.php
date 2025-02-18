7b1f4142bdec4b310afb11e381038f799655159c
GE08T <gaerthamgk@gmail.com>
2025-02-18 23:39:06 +0700

    Menampilkan View dari Controller, Menampilkan Data Ke View, Meneruskan Data Ke View

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,4 +9,10 @@ class WelcomeController extends Controller
     public function helo() {
         return 'Hello World';
     }
+
+    public function greeting() {
+        return view ('blog.hello')
+        ->with('name','Galung Erlyan Tama')
+        ->with('occupation', 'Astronaut');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -42,3 +42,5 @@ Route::resource('photos', PhotoController::class)
 ->except([ 
     'create', 'store', 'update', 'destroy' 
 ]); 
+
+Route::get('/greeting', [WelcomeController::class, 'greeting']); 
